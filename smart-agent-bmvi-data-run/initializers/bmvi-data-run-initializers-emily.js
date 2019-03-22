db6beaa3ba2865d32895a66088a22b65e964f53d
wulfraem <wulfraem@users.noreply.github.com>
2019-03-22 16:15:22 +0100

    add start for streaming car data from csv files

--- smart-agent-bmvi-data-run/initializers/bmvi-data-run-initializers-emily.js
+++ smart-agent-bmvi-data-run/initializers/bmvi-data-run-initializers-emily.js
@@ -1,15 +1,18 @@
 'use strict'
 
+const csv = require('csv-parser')
+const fs = require('fs')
+
 const {
   api,
   Initializer,
 } = require('actionhero')
 
 
-module.exports = class SmartAgentBmviDataRunInitializer extends Initializer {
+module.exports = class SmartAgentBmviDataRunInitializerEmily extends Initializer {
   constructor() {
     super()
-    this.name = 'bmvi-data-run'
+    this.name = 'bmvi-data-run-emily'
     this.loadPriority = 4100
     this.startPriority = 4100
     this.stopPriority = 4100
@@ -21,17 +24,64 @@ module.exports = class SmartAgentBmviDataRunInitializer extends Initializer {
     }
 
     // specialize from blockchain smart agent library
-    class SmartAgentBmviDataRun extends api.smartAgents.SmartAgent {
+    class SmartAgentBmviDataRunEmily extends api.smartAgents.SmartAgent {
       async initialize () {
         await super.initialize()
+        this.streams = {}
+      }
+
+      async startStreaming () {
+        const emilies = await this._getEmilies()
+        for (let contractId of Object.keys(emilies)) {
+          try {
+            const data = await this._getCsvData(`${__dirname}/../csv/${emilies[contractId]}`)
+            // const streamId = await this.runtime.dataContract.getEntry(
+            //   contractId, 'streamId', this.config.ethAccount)
+            // const { public: { name } } = await this.runtime.description.getDescription(contractId)
+            let cursor = 0;
+            setInterval(() => {
+              // pretend, data is from now
+              const row = { ...data[cursor], last_seen: Date.now() }
+              // api.streamer
+              //   .push(streamId, name, row)
+              //   .catch(error => api.log(error, 'error'))
+              console.log(`--- ${contractId} -----------------------------------------------------`)
+              console.log(row)
+              console.log('-----------------------------------------------------------------------')
+              if (cursor++ >= data.length) {
+                cursor = 0;
+              }
+            }, this.config.streamInterval)
+          } catch (ex) {
+            api.log(`could not get data for streaming; ${ex.message || ex}`, 'error')
+          }
+        }
+      }
+
+      async _getCsvData (file) {
+        const data = [];
+        await new Promise((resolve, reject) => {
+          fs.createReadStream(file)  
+            .pipe(csv())
+            .on('data', (row) => { data.push(row) })
+            .on('end', () => { resolve(); })
+            .on('error', (error) => { reject(error); })
+        })
+        api.log(`got ${data.length} lines of data`, 'info')
+        return data;
+      }
+
+      async _getEmilies() {
+        return Promise.resolve(this.config.emilies)
       }
     }
 
     // start the initialization code
-    const smartAgentBmviDataRun = new SmartAgentBmviDataRun(api.config.smartAgentBmviDataRun)
-    await smartAgentBmviDataRun.initialize()
+    const smartAgentBmviDataRunEmily = new SmartAgentBmviDataRunEmily(api.config.smartAgentBmviDataRun)
+    await smartAgentBmviDataRunEmily.initialize()
+    await smartAgentBmviDataRunEmily.startStreaming()
 
     // objects and values used outside initializer
-    api.smartAgentBmviDataRun = smartAgentBmviDataRun
+    api.smartAgentBmviDataRunEmily = smartAgentBmviDataRunEmily
   }
 }
