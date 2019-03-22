6afddb3f3ddf01bb61f3b76283df9ae413444e1f
S3bb1 <sebastian.dechant@evan.team>
2019-03-22 22:13:31 +0100

    add listener for event watching

--- smart-agent-bmvi-data-run/initializers/bmvi-data-run-initializers-insurance.js
+++ smart-agent-bmvi-data-run/initializers/bmvi-data-run-initializers-insurance.js
@@ -29,6 +29,49 @@ module.exports = class SmartAgentBmvidatarunInsuranceWatcherInitializer extends 
         // when the car is financed check if bank approves
         // then allow the repair
       }
+
+      async startEventWatching() {
+
+        this.managedTwins = api.config.smartAgentBmviDataRunInsurance.cars || []
+        // add DataContract abi to decoder
+        abiDecoder.addABI(JSON.parse(this.runtime.contractLoader.contracts.DataContract.interface))
+
+        api.eth.blockEmitter.on('data', async (block) => {
+          for (let tx of block.transactions) {
+            // check if target of transaction in list of contracts from profile
+            if (this.managedTwins.includes(tx.to)) {
+              const input = abiDecoder.decodeMethod(tx.input)
+              if (input) {
+                // check if target list is 'usagelog'
+                if (input.params[0].value[0] ===
+                    this.runtime.nameResolver.soliditySha3('maintenanceData')) {
+                  // retrieve all entries, starting with newest entry
+                  const entries = await this.runtime.dataContract.getListEntries(
+                    tx.to,
+                    'maintenanceData',
+                    config.ethAccount,
+                    true,
+                    true,
+                    10,
+                    0,
+                    true,
+                  )
+
+                  const financed = await this.runtime.dataContract.getEntry(
+                    tx.to,
+                    'financing',
+                    config.ethAccount
+                  )
+
+                  if(financed) {
+                    // how this will be checked?
+                  }
+                }
+              }
+            }
+          }
+        })
+      }
     }
 
     // start the initialization code
