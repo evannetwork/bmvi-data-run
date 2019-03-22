0d4353c7a3160f93b3b6309daba599ae5c9c3495
Tobias Winkler <tobias.winkler@evan.team>
2019-03-22 19:09:35 +0100

    fix info style

--- dapps/dashboard/src/components/info/info.vue
+++ dapps/dashboard/src/components/info/info.vue
@@ -35,12 +35,12 @@
       </div>
 
       <div class="pl-3 pr-3 text-center">
-        <img class="img-fluid p-3 pl-5 pr-5 col-md-6"
+        <img class="img-fluid p-3 pl-5 pr-5 col-md-8"
           :src="`${ $store.state.bmviBaseUrl }/assets/usecase.png`">
       </div>
     </div>
     <div class="d-flex flex-wrap">
-      <div class="col-md-6 p-3" v-for="(usecase, index) in [ 1,2,3,4,5,6 ]">
+      <div class="pl-3 mb-3 col-md-6" v-for="(usecase, index) in [ 1,2,3,4,5,6 ]">
         <div class="bg-level-1 border">
           <div class="d-flex p-2 pt-3 pb-3 border-bottom">
             <h4 class="m-0 ml-3">
