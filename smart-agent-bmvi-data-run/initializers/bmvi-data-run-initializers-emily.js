e4b0f3623fa1694dc5794b167d989cea9b9ea32e
wulfraem <wulfraem@users.noreply.github.com>
2019-03-23 10:32:16 +0100

    add moving emily to maintenace; use customer account for emily

--- smart-agent-bmvi-data-run/initializers/bmvi-data-run-initializers-emily.js
+++ smart-agent-bmvi-data-run/initializers/bmvi-data-run-initializers-emily.js
@@ -29,6 +29,53 @@ module.exports = class SmartAgentBmviDataRunInitializerEmily extends Initializer
       async initialize () {
         await super.initialize()
         this.streams = {}
+        this.maintenances = {}
+        this.cursors = {}
+      }
+
+      async startMaintenanceWatcher() {
+        this.managedTwins = Object.keys(api.config.smartAgentBmviDataRunEmily.emilies)
+        // add DataContract abi to decoder
+        abiDecoder.addABI(JSON.parse(this.runtime.contractLoader.contracts.DataContract.interface))
+
+        api.eth.blockEmitter.on('data', async (block) => {
+          for (let tx of block.transactions) {
+            if (tx.from === this.config.ethAccount) {
+              // ignore own entries
+              continue;
+            }
+            // check if target of transaction in list of contracts from profile
+            if (this.managedTwins.includes(tx.to)) {
+              const input = abiDecoder.decodeMethod(tx.input)
+              if (input) {
+                // check if target list is 'usagelog'
+                if (input.params[0].value[0] ===
+                    this.runtime.nameResolver.soliditySha3('maintenanceData')) {
+                  api.log('fetching maintenanceData', 'debug')
+                  // retrieve all entries, starting with newest entry
+                  const entries = await this.runtime.dataContract.getListEntries(
+                    tx.to,
+                    'maintenanceData',
+                    this.config.ethAccount,
+                    true,
+                    true,
+                    1,
+                    0,
+                    true,
+                  )
+
+                  if (entries.filter(entry => entry.maintenanceApproved).length) {
+                    api.log('found maintenance approval', 'debug')
+                    this.maintenances[tx.to] = true
+                  } else if (entries.filter(entry => entry.maintenanceFinished).length) {
+                    api.log('found maintenance finished', 'debug')
+                    this.maintenances[tx.to] = false
+                  }
+                }
+              }
+            }
+          }
+        })
       }
 
       async startStreaming () {
@@ -36,9 +83,7 @@ module.exports = class SmartAgentBmviDataRunInitializerEmily extends Initializer
         for (let contractId of Object.keys(emilies)) {
           try {
             const data = await this._getCsvData(`${__dirname}/../csv/${emilies[contractId]}`)
-            // const streamId = await this.runtime.dataContract.getEntry(
-            //    contractId, 'streamId', this.config.ethAccount)
-            const streamId = contractId
+
             // approved interval
             let approved = false
             setInterval(async () => {
@@ -51,20 +96,40 @@ module.exports = class SmartAgentBmviDataRunInitializerEmily extends Initializer
                   ex.message || ex, 'error')
               }
             }, this.config.appvovalCheckInterval)
+
+            // fade coordinates to maintenance point
+            let maintenanceFade = 0
+            setInterval(async () => {
+              if (this.maintenances[contractId]) {
+                maintenanceFade += this.config.maintenanceFadeStep
+                if (maintenanceFade >= 1) {
+                  maintenanceFade = 1
+                  this.cursors[contractId] = 0
+                }
+              } else {
+                maintenanceFade = 0
+              }
+            }, this.config.maintenanceFadeInterval)
+
+
             // stream interval
-            let cursor = 0
+            this.cursors[contractId] = 0
             setInterval(async () => {
               // only stream if approved
               if (approved) {
                 // pretend, data is from now
-                const row = { ...data[cursor], last_seen: Date.now() }
+                const row = { ...data[this.cursors[contractId]], last_seen: Date.now() }
+                row.longitude = row.longitude * (1 - maintenanceFade) + this.config.maintenanceX * maintenanceFade
+                row.latitude = row.latitude * (1 - maintenanceFade) + this.config.maintenanceY * maintenanceFade
                 try {
-                  await api.streamr.addToStream(streamId, row)
+                  await api.streamr.addToStream(contractId, row)
                 } catch (ex) {
-
+                  api.log(`could not stream data for ${contractId}; ${ex.message || ex}`, 'error')
                 }
-                if (cursor++ >= data.length) {
-                  cursor = 0;
+                if (!this.maintenances[contractId]) {
+                  if (this.cursors[contractId]++ >= data.length) {
+                    this.cursors[contractId] = 0;
+                  }
                 }
               }
             }, this.config.streamInterval)
@@ -96,6 +161,7 @@ module.exports = class SmartAgentBmviDataRunInitializerEmily extends Initializer
     const smartAgentBmviDataRunEmily = new SmartAgentBmviDataRunEmily(api.config.smartAgentBmviDataRunEmily)
     await smartAgentBmviDataRunEmily.initialize()
     await smartAgentBmviDataRunEmily.startStreaming()
+    await smartAgentBmviDataRunEmily.startMaintenanceWatcher()
 
     // objects and values used outside initializer
     api.smartAgentBmviDataRunEmily = smartAgentBmviDataRunEmily
