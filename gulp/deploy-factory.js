0034f3c729b40c71d97317808c24d4c6c6685f55
S3bb1 <sebastian.dechant@evan.team>
2019-07-05 13:46:09 +0200

    add support for web3 beta 55
    CORE-223

--- gulp/deploy-factory.js
+++ gulp/deploy-factory.js
@@ -1,12 +1,12 @@
 /*
   Copyright (c) 2018-present evan GmbH.
- 
+
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
   You may obtain a copy of the License at
- 
+
       http://www.apache.org/licenses/LICENSE-2.0
- 
+
   Unless required by applicable law or agreed to in writing, software
   distributed under the License is distributed on an "AS IS" BASIS,
   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,8 +36,9 @@ let dfs;
 let runtimes;
 
 gulp.task('init', async () => {
-  web3 = new Web3(new Web3.providers.WebsocketProvider(
-    runtimeConfig.web3Provider, { clientConfig: { keepalive: true, keepaliveInterval: 5000 } }));
+  const wsp = new Web3.providers.WebsocketProvider(
+       runtimeConfig.web3Provider, { clientConfig: { keepalive: true, keepaliveInterval: 5000 } });
+  web3 = new Web3(wsp, null, { transactionConfirmationBlocks: 1 });
 
   runtimeConfig.additionalContractsPaths = [`${__dirname}/../contracts`];
 
